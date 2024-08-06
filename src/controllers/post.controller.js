4eb65dd63cffd03a1e927c54974ba613dffc350f
Harsh Balwani <balwaniharsh1001@gmail.com>
2024-08-06 10:55:04 +0530

    done whatever i was capable to do

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,4 +1,5 @@
 const asyncHandler = require("../utils/asyncHandler");
+const { Op } = require("sequelize");
 const ApiError = require("../utils/apiError");
 const ApiResponse = require("../utils/apiResponse");
 const { Post, User } = require("../models/index");
@@ -58,10 +59,127 @@ const getAllPosts = asyncHandler(async (req, res) => {
 
   return res
     .status(200)
+    .json(new ApiResponse(200, posts, "Got all the shorts successfully"));
+});
+
+const getFilteredPosts = asyncHandler(async (req, res) => {
+  const { category, publish_date, upvotes } = req.query;
+
+  const where = {};
+
+  if (category) {
+    where.category = category;
+  }
+  // will try later
+  // found from stackoverflow but not working as expected
+  if (publish_date) {
+    where.publish_date = {
+      [sequelize.Op.gte]: new Date(publish_date),
+    };
+  }
+
+  // found from stackoverflow but not working as expected
+  if (upvotes) {
+    where[sequelize.literal("JSON_EXTRACT(votes, '$.upvote')")] = {
+      [sequelize.Op.gt]: parseInt(upvotes, 10),
+    };
+  }
+
+  const posts = await Post.findAll({
+    where,
+    order: [
+      ["publish_date", "DESC"],
+      [sequelize.literal("JSON_EXTRACT(votes, '$.upvote')"), "DESC"],
+    ],
+  });
+
+  return res
+    .status(200)
     .json(new ApiResponse(200, posts, "Posts retrieved successfully"));
+});
+
+// const searchPosts = asyncHandler(async (req, res) => {
+//   const { title, keyword, author } = req.query;
+//   const where = {};
+
+//   if (title) {
+//     where.title = {
+//       [Op.like]: `%${title}%`,
+//     };
+//   }
+
+//   if (keyword) {
+//     where[Op.or] = [
+//       { title: { [Op.like]: `%${keyword}%` } },
+//       { content: { [Op.like]: `%${keyword}%` } },
+//     ];
+//   }
+
+//   if (author) {
+//     where.author = {
+//       [Op.like]: `%${author}%`,
+//     };
+//   }
+
+//   const posts = await Post.findAll({
+//     where,
+//     order: [
+//       ["publish_date", "DESC"],
+//       [sequelize.literal("JSON_EXTRACT(votes, '$.upvote')"), "DESC"],
+//     ],
+//   });
+
+//   return res
+//     .status(200)
+//     .json(new ApiResponse(200, posts, "Search results retrieved successfully"));
+// });
+
+const searchPosts = asyncHandler(async (req, res) => {
+  const { title, keyword, author } = req.query;
+  const where = {};
+
+  if (title) {
+    where.title = {
+      [Op.like]: `%${title}%`,
+    };
+  }
+
+  if (keyword) {
+    where[Op.or] = [
+      { title: { [Op.like]: `%${keyword}%` } },
+      { content: { [Op.like]: `%${keyword}%` } },
+    ];
+  }
+
+  if (author) {
+    where.author = {
+      [Op.like]: `%${author}%`,
+    };
+  }
+
+ 
+  if (Object.keys(where).length === 0) {
+    return res
+      .status(400)
+      .json(new ApiResponse(400, [], "No search criteria provided"));
+  }
+
+  const posts = await Post.findAll({
+    where,
+    order: [
+      ["publish_date", "DESC"],
+      [sequelize.literal("JSON_EXTRACT(votes, '$.upvote')"), "DESC"],
+    ],
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, posts, "Search results retrieved successfully"));
 });
 
 module.exports = {
   createPost,
   getAllPosts,
+  getFilteredPosts,
+  searchPosts,
 };
--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { createPost, getAllPosts } = require("../controllers/post.controller");
+const {
+  createPost,
+  getAllPosts,
+  getFilteredPosts,
+  searchPosts,
+} = require("../controllers/post.controller");
 
 const verifyJWT = require("../middlewares/auth.middleware");
 
 router.route("/create").post(verifyJWT, createPost);
 router.route("/feed").get(verifyJWT, getAllPosts);
+router.route("/filter").get(verifyJWT, getFilteredPosts);
+router.route("/filter").get(verifyJWT, searchPosts);
 
 module.exports = router;
